a2a4aec80470f98caac338fd771555a81cecb1f5
Vladan Stojakovic <vladan.stojakovic553@gmail.com>
2026-05-04 20:08:51 +0200

    Dodata podrška za RemoteWebDriver i novi testng-fastRemote.xml

--- src/main/java/utils/WebDrivers.java
+++ src/main/java/utils/WebDrivers.java
@@ -1,48 +1,80 @@
 package utils;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
-
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class WebDrivers {
+    
+    // Umesto hardkodovanja, čitamo iz Environment varijabli (bitno za GitHub Actions)
+    static String userName = System.getenv("BROWSERSTACK_USERNAME");
+    static String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    static String cloudUrl = "https://" + userName + ":" + accessKey + "@hub.browserstack.com/wd/hub";
 
-	
-	static String geckoPath = "C:\\Users\\vladan\\eclipse-workspace\\elevationCounterTest\\geckodriver.exe";
-	static String webDriverProperty = "webdriver.gecko.driver";
-	
-	public static WebDriver createDriver(BrowserType browserType) {
-		WebDriver driver = null;
+    public static WebDriver createDriver(BrowserType browserType) {
+        WebDriver driver = null;
 
-		switch (browserType) {
-		case EDGE:
-			System.setProperty("edge will not be used :(", "");
-			driver = new EdgeDriver();
-			break;
+        switch (browserType) {
+        case EDGE:
+            // Selenium Manager (verzija 4.6.0+) automatski nalazi drajver, 
+            // pa ti System.setProperty više i ne treba ako imaš noviji Selenium.
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--start-maximized");
+            driver = new EdgeDriver(edgeOptions);
+            break;
 
-		case FIREFOX:
-			System.setProperty(webDriverProperty,geckoPath);
-			FirefoxOptions firefoxOptions = new FirefoxOptions();
-			firefoxOptions.addArguments("--start-maximized");
-			firefoxOptions.addArguments("--disable-popup-blocking");
-			firefoxOptions.addArguments("--disable-notifications");
-			firefoxOptions.addPreference("dom.disable_open_during_load", true);
-			firefoxOptions.addPreference("dom.popup_maximum", 0);
-			firefoxOptions.addPreference("permissions.default.desktop-notification", 2);
+        case FIREFOX:
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--start-maximized");
+            driver = new FirefoxDriver(firefoxOptions);
+            break;
 
-			driver = new FirefoxDriver(firefoxOptions);
-			break;
+        default:
+            throw new IllegalArgumentException("Browser type not supported: " + browserType);
+        }
+        return driver;
+    }
 
-		default:
-			throw new IllegalArgumentException("Browser type not supported: " + browserType);
-		}
-		return driver;
-	}
+    public static WebDriver createRemoteDriver(BrowserType browserType) {
+        MutableCapabilities capabilities;
 
-	public static enum BrowserType {
-		EDGE, FIREFOX
-	}
+        switch (browserType) {
+            case EDGE:
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                capabilities = edgeOptions;
+                break;
+            case FIREFOX:
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--start-maximized");
+                capabilities = firefoxOptions;
+                break;
+            default:
+                throw new IllegalArgumentException("Remote browser type not supported: " + browserType);
+        }
 
+        Map<String, Object> bstackOptions = new HashMap<>();
+        bstackOptions.put("os", "Windows");
+        bstackOptions.put("osVersion", "11");
+        bstackOptions.put("projectName", "Moj Projekt");
+        capabilities.setCapability("bstack:options", bstackOptions);
+
+        try {
+            return new RemoteWebDriver(new URL(cloudUrl), capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Neispravan Cloud URL. Proveri environment varijable!");
+        }
+    }
+
+    public static enum BrowserType {
+        EDGE, FIREFOX
+    }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -14,40 +14,48 @@ public class BaseTest {
     protected static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
     protected static ExtentReports extent = ExtentManager.getInstance();
 
-    // 1. Pomoćna metoda koja samo otvara browser (bez pravljenja testa u reportu)
-    private void openBrowser(String browser) {
-        WebDriver driver = WebDrivers.createDriver(browser.equalsIgnoreCase("firefox") ? 
-                          WebDrivers.BrowserType.FIREFOX : WebDrivers.BrowserType.EDGE);
+    // 1. Modifikovana pomoćna metoda koja podržava i Remote i Local
+    private void openBrowser(String browser, String executionType) {
+        WebDriver driver;
+        WebDrivers.BrowserType type = browser.equalsIgnoreCase("firefox") ? 
+                                      WebDrivers.BrowserType.FIREFOX : WebDrivers.BrowserType.EDGE;
+
+        // Provera gde se test izvršava
+        if ("remote".equalsIgnoreCase(executionType)) {
+            driver = WebDrivers.createRemoteDriver(type);
+        } else {
+            driver = WebDrivers.createDriver(type);
+        }
+
         threadDriver.set(driver);
         getDriver().manage().window().maximize();
     }
 
-    // 2. Pokretanje browsera JEDNOM za celu klasu (Fast verzija)
+    // 2. Setup za celu klasu (Fast verzija)
     @BeforeClass
-    @Parameters({"browser", "sessionType"})
-    public void setupClass(String browser, @Optional("method") String sessionType) {
+    @Parameters({"browser", "sessionType", "executionType"})
+    public void setupClass(String browser, @Optional("method") String sessionType, @Optional("local") String executionType) {
         if (sessionType.equalsIgnoreCase("class")) {
-            openBrowser(browser);
+            openBrowser(browser, executionType);
         }
     }
 
-    // 3. OVO JE KLJUČ: Pravljenje zapisa u reportu za SVAKU metodu, u oba scenarija
+    // 3. Setup za svaku metodu (Pravljenje reporta i opciono otvaranje browsera)
     @BeforeMethod
-    @Parameters({"browser", "sessionType"})
-    public void setupMethod(String browser, @Optional("method") String sessionType, Method method) {
-        // Izvlačimo opis
+    @Parameters({"browser", "sessionType", "executionType"})
+    public void setupMethod(String browser, @Optional("method") String sessionType, @Optional("local") String executionType, Method method) {
         Test testAnnotation = method.getAnnotation(Test.class);
         String description = (testAnnotation != null) ? testAnnotation.description() : "Nema opisa";
-        String testDisplayName = method.getName() + " [" + browser.toUpperCase() + "] - " + description;
+        
+        // Dodajemo informaciju o tipu izvršavanja u ime testa u reportu
+        String testDisplayName = method.getName() + " [" + browser.toUpperCase() + " - " + executionType.toUpperCase() + "] - " + description;
 
-        // Pravimo novi test u Extent Reportu
         ExtentTest test = extent.createTest(testDisplayName);
         extentTest.set(test);
 
-        // Ako je sesija podešena na "method", ovde otvaramo browser (Isolated verzija)
         if (sessionType.equalsIgnoreCase("method")) {
-            openBrowser(browser);
-            extentTest.get().info("Browser pokrenut (Nova sesija)");
+            openBrowser(browser, executionType);
+            extentTest.get().info("Browser pokrenut (" + executionType + " sesija)");
         } else {
             extentTest.get().info("Nastavak u postojećoj sesiji (Fast mode)");
         }
@@ -58,7 +66,6 @@ public class BaseTest {
     @AfterMethod
     @Parameters("sessionType")
     public void tearDownMethod(@Optional("method") String sessionType) {
-        // Zatvaramo drajver posle svake metode SAMO ako je Isolated mod
         if (sessionType.equalsIgnoreCase("method") && getDriver() != null) {
             closeDriver();
         }
@@ -67,14 +74,15 @@ public class BaseTest {
     @AfterClass
     @Parameters("sessionType")
     public void tearDownClass(@Optional("method") String sessionType) {
-        // Zatvaramo drajver na kraju klase SAMO ako je Fast mod
         if (sessionType.equalsIgnoreCase("class") && getDriver() != null) {
             closeDriver();
         }
     }
 
     private void closeDriver() {
-        getDriver().quit();
+        if (getDriver() != null) {
+            getDriver().quit();
+        }
         threadDriver.remove();
         extentTest.remove();
     }
